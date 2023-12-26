86dc5fa10310db9354e936d86b5ab89e8707bd77
CodeSandbox <support@codesandbox.io>
2023-12-26 07:37:56 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Navbar from "./componnents/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello worldb! </h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/componnents/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="menu">
+      <li>
+        <ul>Mes diplomes</ul>
+        <ul>Mes projets</ul>
+        <ul>Contact</ul>
+      </li>
+    </div>
+  );
+}
