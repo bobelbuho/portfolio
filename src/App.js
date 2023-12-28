f8af556a80db65d9b24bd52b2d036d58dcddba6b
El Buho <95287495+bobelbuho@users.noreply.github.com>
2023-12-28 15:30:53 +0000

    Update 5 files FOOTER

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import Navbar from "./componnents/Navbar";
+import Footer from "./componnents/Footer";
 
 export default function App() {
   return (
@@ -7,6 +8,8 @@ export default function App() {
       <Navbar />
       <h1>Bob VAZ </h1>
       <h2>Developpeur Web Full Stack</h2>
+
+      <Footer />
     </div>
   );
 }
