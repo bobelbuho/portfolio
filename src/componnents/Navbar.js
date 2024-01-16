47f2de906ab6004cf1d6ae9c9536e4a5f863a30d
Bob <bob.vaz@hotmail.fr>
2024-01-16 19:55:52 +0100

    bio

--- src/componnents/Navbar.js
+++ src/componnents/Navbar.js
@@ -2,9 +2,9 @@ export default function Navbar() {
   return (
     <div className="menu">
       <li>
-        <ul>Mes diplomes</ul>
-        <ul>Mes projets</ul>
-        <ul>Contact</ul>
+        <ul><a href="#">Mes diplomes</a></ul>
+        <ul><a href="#">Mes projets</a></ul>
+        <ul><a href="#">Contact</a></ul>
       </li>
     </div>
   );
