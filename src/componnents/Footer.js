97c8776387b710d4671d0be35753896492a3878c
Bob <bob.vaz@hotmail.fr>
2024-01-17 22:15:04 +0100

    footer uptdate

--- src/componnents/Footer.js
+++ src/componnents/Footer.js
@@ -3,7 +3,12 @@ export default function Footer() {
     <div className="footer">
       <li>
         <ul>footer</ul>
+        <ul>
+          <a href="#">Contactez-moi dès maintenant</a>
+        <a href="https://www.linkedin.com/in/bob-vaz/">Linkedin</a>
+      </ul>
       </li>
+      
     </div>
   );
 }
