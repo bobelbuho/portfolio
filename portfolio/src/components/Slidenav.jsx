889070ad1b411154c9b36509e55641f9af457dcc
Bob <bob.vaz@hotmail.fr>
2024-01-24 01:58:23 +0100

    nav

--- portfolio/src/components/Slidenav.jsx
+++ portfolio/src/components/Slidenav.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
 
 const Slidenav = () => {
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
+
+
     return (
-        <div>
-            
-        </div>
+        <header>
+            <h1>Logo</h1>
+            <nav ref={navRef}>
+                <a href="#">Acceuil</a>
+                <a href="#">Mes projets</a>
+                <a href="#">Contact</a>
+                <button className='nav-btn nav-class-btn' onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className='nav-btn' onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
     );
 };
 
